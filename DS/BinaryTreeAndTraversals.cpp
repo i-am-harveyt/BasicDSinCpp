901ab280b1416e10bfd029746b01fe105f6eab40
Hao-Ting Tung <haotingtong30@gmail.com>
2022-06-30 21:50:13 +0800

    Tackle on Thread BT, but encounter problems

--- DS/BinaryTreeAndTraversals.cpp
+++ DS/BinaryTreeAndTraversals.cpp
@@ -3,159 +3,137 @@
 #include <time.h>
 
 // Node
-class Node
-{
+class Node {
 public:
-    int data;
-    Node *left;
-    Node *right;
-    Node(int data)
-    {
-        this->data = data;
-        this->left = this->right = NULL;
-    }
+  int data;
+  Node *left;
+  Node *right;
+  Node(int data) {
+    this->data = data;
+    this->left = this->right = NULL;
+  }
 };
 
-class Queue
-{
+class Queue {
 private:
-    int front;
-    int rear;
-    int length;
-    Node **items;
+  int front;
+  int rear;
+  int length;
+  Node **items;
 
 public:
-    Queue(int length)
-    {
-        this->items = new Node *[length + 1];
-        this->front = 0;
-        this->rear = 0;
-        this->length = length + 1;
+  Queue(int length) {
+    this->items = new Node *[length + 1];
+    this->front = 0;
+    this->rear = 0;
+    this->length = length + 1;
+  }
+  void enqueue(Node *item) {
+    this->front = (this->front + 1) % this->length;
+    if (this->front == this->rear) {
+      std::cout << "Queue is full" << std::endl;
+      this->front = (this->front + this->length - 1) % this->length;
+      return;
     }
-    void enqueue(Node *item)
-    {
-        this->front = (this->front + 1) % this->length;
-        if (this->front == this->rear)
-        {
-            std::cout << "Queue is full" << std::endl;
-            this->front = (this->front + this->length - 1) % this->length;
-            return;
-        }
-        this->items[this->front] = item;
+    this->items[this->front] = item;
+  }
+  Node *dequeue(void) {
+    if (this->rear >= this->front) {
+      std::cout << "Queue is empty" << std::endl;
+      this->rear = (this->rear + this->length - 1) % this->length;
+      return NULL;
     }
-    Node *dequeue(void)
-    {
-        if (this->rear >= this->front)
-        {
-            std::cout << "Queue is empty" << std::endl;
-            this->rear = (this->rear + this->length - 1) % this->length;
-            return NULL;
-        }
-        this->rear = (this->rear + 1) % this->length;
-        return this->items[this->rear];
-    }
-    void all(void)
-    {
-        for (int i = 0; i < this->length; i++)
-            if (this->items[i] != NULL)
-                std::cout << (this->items[i])->data << " ";
-        std::cout << std::endl;
-    }
+    this->rear = (this->rear + 1) % this->length;
+    return this->items[this->rear];
+  }
+  void all(void) {
+    for (int i = 0; i < this->length; i++)
+      if (this->items[i] != NULL)
+        std::cout << (this->items[i])->data << " ";
+    std::cout << std::endl;
+  }
 };
 
 // Tree part
-class BT
-{
+class BT {
 public:
-    Node *root;
-    BT(Queue q)
-    {
-        this->root = NULL;
-        genNewTree(q);
-    }
-    void genNewTree(Queue q)
-    {
-        Node *root = new Node(0);
-        this->root = root;
-        q.enqueue(root);
-        // set seed for ran() or it will return same sequence
-        srand(time(0));
-        for (int i = 1; i < 7; i++)
-        {
-            Node *current = q.dequeue();
-            int param = rand() % 100 % 2;
+  Node *root;
+  BT(Queue q) {
+    this->root = NULL;
+    genNewTree(q);
+  }
+  void genNewTree(Queue q) {
+    Node *root = new Node(0);
+    this->root = root;
+    q.enqueue(root);
+    // set seed for ran() or it will return same sequence
+    srand(time(0));
+    for (int i = 1; i < 7; i++) {
+      Node *current = q.dequeue();
+      int param = rand() % 100 % 2;
 
-            Node *newNode = new Node(i);
-            if (param == 0)
-            {
-                if (current->left == NULL)
-                    current->left = newNode;
-                else
-                    current->right = newNode;
-            }
-            else if (param == 1)
-            {
-                if (current->right == NULL)
-                    current->right = newNode;
-                else
-                    current->left = newNode;
-            }
-            else
-            {
-                q.enqueue(current);
-                continue;
-            }
-            if (!(current->left && current->right))
-                q.enqueue(current);
-            q.enqueue(newNode);
-        }
+      Node *newNode = new Node(i);
+      if (param == 0) {
+        if (current->left == NULL)
+          current->left = newNode;
+        else
+          current->right = newNode;
+      } else if (param == 1) {
+        if (current->right == NULL)
+          current->right = newNode;
+        else
+          current->left = newNode;
+      } else {
+        q.enqueue(current);
+        continue;
+      }
+      if (!(current->left && current->right))
+        q.enqueue(current);
+      q.enqueue(newNode);
     }
+  }
 };
 
-void preorder(Node *current)
-{
-    if (current == NULL)
-        return;
-    std::cout << current->data << " ";
-    if (current->left)
-        preorder(current->left);
-    if (current->right)
-        preorder(current->right);
+void preorder(Node *current) {
+  if (current == NULL)
+    return;
+  std::cout << current->data << " ";
+  if (current->left)
+    preorder(current->left);
+  if (current->right)
+    preorder(current->right);
 }
-void inorder(Node *current)
-{
-    if (current == NULL)
-        return;
-    if (current->left)
-        inorder(current->left);
-    std::cout << current->data << " ";
-    if (current->right)
-        inorder(current->right);
+void inorder(Node *current) {
+  if (current == NULL)
+    return;
+  if (current->left)
+    inorder(current->left);
+  std::cout << current->data << " ";
+  if (current->right)
+    inorder(current->right);
 }
-void postorder(Node *current)
-{
-    if (current == NULL)
-        return;
-    if (current->left)
-        postorder(current->left);
-    if (current->right)
-        postorder(current->right);
-    std::cout << current->data << " ";
+void postorder(Node *current) {
+  if (current == NULL)
+    return;
+  if (current->left)
+    postorder(current->left);
+  if (current->right)
+    postorder(current->right);
+  std::cout << current->data << " ";
 }
 
-int main(void)
-{
-    Queue queue = Queue(20);
-    BT tree = BT(queue);
-    std::cout << "Preorder: ";
-    preorder(tree.root);
-    std::cout << std::endl;
-    std::cout << "Inorder: ";
-    inorder(tree.root);
-    std::cout << std::endl;
-    std::cout << "Postorder: ";
-    postorder(tree.root);
-    std::cout << std::endl;
+int main(void) {
+  Queue queue = Queue(20);
+  BT tree = BT(queue);
+  std::cout << "Preorder: ";
+  preorder(tree.root);
+  std::cout << std::endl;
+  std::cout << "Inorder: ";
+  inorder(tree.root);
+  std::cout << std::endl;
+  std::cout << "Postorder: ";
+  postorder(tree.root);
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
---
+++ DS/ThreadBT.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <vector>
+
+class Node {
+public:
+  int value;
+  Node *left, *right;
+  Node(int val) {
+    value = val;
+    left = nullptr, right = nullptr;
+  }
+};
+
+class BST {
+public:
+  Node *root;
+  int numOfNodes;
+  int size(void) { return numOfNodes; }
+
+  BST(void) {
+    root = nullptr;
+    numOfNodes = 0;
+  }
+  void genNewTree(std::vector<int> &nums) {
+    int i = 1;
+    numOfNodes = nums.size();
+    root = new Node(nums[0]);
+    Node *cur;
+    while (i < numOfNodes) {
+      cur = root;
+      while (true) {
+        if (nums[i] <= cur->value) {
+          if (cur->left == nullptr) {
+            cur->left = new Node(nums[i++]);
+            break;
+          } else {
+            cur = cur->left;
+            continue;
+          }
+        } else {
+          if (cur->right == nullptr) {
+            cur->right = new Node(nums[i++]);
+            break;
+          } else {
+            cur = cur->right;
+            continue;
+          }
+        }
+      }
+    }
+  }
+  void all(void) {
+    Node *cur = root;
+    std::vector<Node *> stack;
+    while (stack.size() || cur) {
+      while (cur) {
+        stack.push_back(cur);
+        cur = cur->left;
+      }
+
+      cur = stack.back();
+      std::cout << cur->value << " ";
+      stack.pop_back();
+
+      cur = cur->right;
+    }
+    std::cout << std::endl;
+  }
+};
+
+class TNode {
+public:
+  int value;
+  TNode *left, *right;
+  bool leftNull, rightNull;
+  TNode(int val) {
+    value = val;
+    left = nullptr, right = nullptr;
+    leftNull = true, rightNull = true;
+  }
+};
+
+class ThreadBT {
+public:
+  TNode *head;
+  ThreadBT() { head = new TNode(0); }
+
+  TNode *insuc(TNode *x) {
+    TNode *temp = x->right;
+    if (!x->rightNull)
+      while (!temp->leftNull)
+        temp = temp->left;
+    return temp;
+  }
+  TNode *inpre(TNode *x) {
+    TNode *temp = x->left;
+    if (!x->leftNull)
+      while (!temp->rightNull)
+        temp = temp->right;
+    return temp;
+  }
+  void insertRight(TNode *node, TNode *newNode) {
+    newNode->right = node->right;
+    newNode->rightNull = node->rightNull;
+    newNode->left = node->left;
+    newNode->leftNull = node->leftNull;
+    node->rightNull = false;
+    node->right = newNode;
+    if (!newNode->rightNull) {
+      TNode *temp = insuc(newNode);
+      temp->left = newNode;
+    }
+  }
+  void insertLeft(TNode *node, TNode *newNode) {
+    newNode->left = node->left;
+    newNode->leftNull = node->leftNull;
+    newNode->right = node->right;
+    newNode->rightNull = node->rightNull;
+    node->leftNull = false;
+    node->left = newNode;
+    if (!newNode->leftNull) {
+      TNode *temp = inpre(newNode);
+      temp->right = newNode;
+    }
+  }
+
+  void copyBST(BST tree) {
+    std::vector<Node *> st;
+    std::vector<TNode *> stt;
+    Node *cur = tree.root, *next;
+    TNode *curT = new TNode(cur->value), *nextT;
+    std::cout << curT->value << std::endl;
+    head->leftNull = false;
+    head->left = curT;
+
+    while (!st.empty() || cur) {
+
+      while (cur) {
+        st.push_back(cur);
+        stt.push_back(curT);
+        cur = cur->left;
+        if (cur) {
+          curT->left = new TNode(cur->value);
+          curT = curT->left;
+        }
+      }
+
+      cur = st.back();
+      curT = stt.back();
+      st.pop_back();
+      stt.pop_back();
+
+      cur = cur->right;
+      if (cur) {
+        curT->right = new TNode(cur->value);
+        curT = curT->right;
+      }
+    }
+    std::cout << "";
+  }
+
+  void linking() {
+    std::vector<TNode *> st;
+    std::vector<TNode *> order;
+    TNode *cur = head->left;
+    st.push_back(cur);
+
+    // in order
+    while (!st.empty() || cur) {
+      while (cur) {
+        st.push_back(cur);
+        cur = cur->left;
+      }
+
+      cur = st.back();
+      st.pop_back();
+      order.push_back(cur);
+
+      cur = cur->right;
+    }
+
+    // link all in order
+    TNode *headNode = order.front(), *tailNode = order.back();
+    headNode->left = head;
+    tailNode->right = head;
+    int size = order.size();
+    for (int i = 1; i < size - 1; i++) {
+      if (order[i]->leftNull)
+        order[i]->left = order[i - 1];
+      if (order[i]->rightNull)
+        order[i]->right = order[i + 1];
+    }
+  }
+
+  void all(void) {
+    TNode *leftist = head->left;
+    while (!leftist->leftNull) {
+      leftist = leftist->left;
+    }
+    while (leftist != head) {
+      std::cout << leftist->value << " ";
+      leftist = insuc(leftist);
+    }
+    std::cout << std::endl;
+  }
+};
+
+int main(void) {
+  BST bst = BST();
+  std::vector<int> nums;
+  nums.push_back(5);
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(6);
+  nums.push_back(3);
+  nums.push_back(5);
+  nums.push_back(7);
+  nums.push_back(8);
+  nums.push_back(3);
+  nums.push_back(6);
+  nums.push_back(9);
+  bst.genNewTree(nums);
+  bst.all();
+  ThreadBT tbt = ThreadBT();
+  tbt.copyBST(bst);
+  tbt.linking();
+  tbt.all();
+  return 0;
+}
