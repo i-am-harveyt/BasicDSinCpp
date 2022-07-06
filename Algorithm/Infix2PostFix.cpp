ee9352d89a1d6a32bb2c7db426481c3ade6cd719
Harvey <haotingtong30@gmail.com>
2022-07-06 20:14:26 +0800

    Infix to Postfix Algorithm, without comment

---
+++ Algorithm/Infix2PostFix.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+int priority(char chr) {
+  if (chr == '+' || chr == '-')
+    return 1;
+  else if (chr == '*' || chr == '/')
+    return 2;
+  else if (chr == '(')
+    return 3;
+  return -1;
+}
+
+void Infix2PostFix(std::string str) {
+  int index = 0, len = 0;
+  char top;
+  std::vector<char> stack;
+  stack.push_back('!');
+  std::set<char> charset;
+  charset.insert('(');
+  charset.insert(')');
+  charset.insert('+');
+  charset.insert('-');
+  charset.insert('*');
+  charset.insert('/');
+
+  while (index < (int)str.size()) {
+    // std::cout << str[index] << std::endl;
+    if (charset.find(str[index]) == charset.cend()) {
+      std::cout << str[index];
+    } else if (str[index] == ')') {
+      while (stack.back() != '(') {
+        std::cout << stack.back();
+        stack.pop_back();
+      }
+      stack.pop_back();
+    } else {
+      while (priority(stack.back()) >= priority(str[index]) &&
+             stack.back() != '(') {
+        std::cout << stack.back();
+        stack.pop_back();
+      }
+      stack.push_back(str[index]);
+    }
+    index++;
+  }
+
+  while (stack.size() > 1) {
+    std::cout << stack.back();
+    stack.pop_back();
+  }
+}
+
+int main(void) {
+  Infix2PostFix("a+b*c");
+  return 0;
+}
